e06bee3db3c225b36e6d356738bc07c3207eb2b0
coderylicode <rylidavis94@gmail.com>
2022-03-30 16:46:45 -0700

    Adding layout margins between card tiles

--- app/src/main/java/com/example/mymemory/MemoryBoardAdapter.kt
+++ app/src/main/java/com/example/mymemory/MemoryBoardAdapter.kt
@@ -10,35 +10,43 @@ import kotlin.math.min
 
 class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
     RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
-        /* a viewholder is an object which provides access to all the views of one recycler view element (one memory card of game) */
+    /* a viewholder is an object which provides access to all the views of one recycler view element (one memory card of game) */
+
+    /*We're going to define a constant which is the margin size.*/
+    companion object {
+        private const val MARGIN_SIZE = 10
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val cardWidth : Int = parent.width / 2
-        val cardHeight : Int = parent.height / 4
-        val cardSideLength : Int = min(cardWidth, cardHeight)
- val view: View = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
-        val layoutParams: ViewGroup.LayoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams
-layoutParams.width = cardSideLength
+        val cardWidth: Int = parent.width / 2 - (2 * MARGIN_SIZE)
+        val cardHeight: Int = parent.height / 4 - (2 * MARGIN_SIZE)
+        val cardSideLength: Int = min(cardWidth, cardHeight)
+        val view: View = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val layoutParams: ViewGroup.MarginLayoutParams =
+            view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.width = cardSideLength
         layoutParams.height = cardSideLength
-return ViewHolder(view)
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
+        return ViewHolder(view)
         /*onCreateViewHolder is responsible for figuring out how to create one view of our recycler view*/
     }
 
     override fun getItemCount() = numPieces
-        // how many elements are in our recycler view and that will be the constructor parameter we passed in, numPieces
+    // how many elements are in our recycler view and that will be the constructor parameter we passed in, numPieces
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-holder.bind(position)
+        holder.bind(position)
         //onBindViewHolder responsible for taking the data at that position and binding it to that ViewHolder passed in here.
     }
 
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind (position: Int) {
+        fun bind(position: Int) {
             //no op
 
         }
-        }
     }
+}
 
 
